905f9c1bdfba31cdba61d2c079c25330b7b0a2a7
mqu <meitongdepaul@gmail.com>
2021-09-10 08:06:31 -0700

    finish backend user auth middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,44 @@
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth  } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+
 const router = require('express').Router();
 
 // !!test route， NEED TO BE CHANGED OR DELETED 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
+
+// !!test route， NEED TO BE CHANGED OR DELETED 
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        },
+    })
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
+
+// !!test route， NEED TO BE CHANGED OR DELETED 
+router.get(
+    '/restore-user',
+    restoreUser,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
+// !!test route， NEED TO BE CHANGED OR DELETED 
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
 
 module.exports = router;
 
