0ba53468a02fc71378d9b02bd1f02ffd263a8a1f
mqu <meitongdepaul@gmail.com>
2021-09-10 11:03:44 -0700

    finish login route at back-end

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,43 +1,23 @@
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie, restoreUser, requireAuth  } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-
-
 const router = require('express').Router();
 
-// !!test route， NEED TO BE CHANGED OR DELETED 
-router.post('/test', function (req, res) {
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+// to delete later
+router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
 });
 
-// !!test route， NEED TO BE CHANGED OR DELETED 
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        },
-    })
-    setTokenCookie(res, user);
-    return res.json({ user });
-}));
 
-// !!test route， NEED TO BE CHANGED OR DELETED 
-router.get(
-    '/restore-user',
-    restoreUser,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
 
-// !!test route， NEED TO BE CHANGED OR DELETED 
-router.get(
-    '/require-auth',
-    requireAuth,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
+
+
+
+
 
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
-// !!test route， NEED TO BE CHANGED OR DELETED 
+// !!test route for scurf token， NEED TO BE CHANGED OR DELETED 
 router.get('/hello/world', function (req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.send('Hello World!');
