bcf380607d90f232a299b89feb7c69028c0479eb
mqu <meitongdepaul@gmail.com>
2021-09-11 22:38:49 -0700

    finish sign-up form sytlings

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
+import './LoginForm.css'
+
+import { Modal } from '../../context/Modal';
+import SignupForm from '../SignupFormModal/SignupForm.js'
+
 
 function LoginForm() {
     const dispatch = useDispatch();
     const [credential, setCredential] = useState("");
     const [password, setPassword] = useState("");
     const [errors, setErrors] = useState([]);
+
+    const [showModal, setShowModal] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,34 +25,69 @@ function LoginForm() {
             }
         );
     };
+    
+    const demonUser=(e)=>{
+        e.preventDefault();
+        return dispatch(sessionActions.login({ credential:'Demo-lition', password:'password' })).catch(
+            async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+            }
+        );
+
+    }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <ul>
-                {errors.map((error, idx) => (
-                    <li key={idx}>{error}</li>
-                ))}
-            </ul>
-            <label>
-                Username or Email
-                <input
-                    type="text"
-                    value={credential}
-                    onChange={(e) => setCredential(e.target.value)}
-                    // required
-                />
-            </label>
-            <label>
-                Password
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    // required
-                />
-            </label>
-            <button type="submit">Log In</button>
-        </form>
+        <>
+            <form className='loginForm' onSubmit={handleSubmit}>
+                <ul className='error'>
+                    {errors.map((error, idx) => (
+                        <li key={idx}>{error}</li>
+                    ))}
+                </ul>
+                <div className='loginInputWrapper'>
+                    <label> 
+                    Username or Email
+                    
+                    <input
+                        className='loginInput'
+                        type="text"
+                        value={credential}
+                        onChange={(e) => setCredential(e.target.value)}
+                        // required
+                    />
+                    </label>
+                </div>
+                <div className='loginInputWrapper'>
+                    <label>
+                        Password
+                        <input
+                            className='loginInput'
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            // required
+                        />
+                    </label>
+                </div>
+                <div className='loginSubmitWrapper'>
+                    <button className='loginSubmitButton' type="submit">Log In</button>
+                    <button className='DemoButton' type="button" onClick={demonUser}>DemoUser</button>
+
+                </div>
+                    
+            </form>
+            
+            <div className='signupWrapper'>
+                <span>Don't have an account?</span>
+                <button className='signupInLogin' onClick={() => setShowModal(true)}>Sign up</button>
+                {showModal && (
+                    <Modal onClose={() => setShowModal(false)}>
+                        <SignupForm />
+                    </Modal>
+                )}
+            </div>
+        </>
     );
 }
 
