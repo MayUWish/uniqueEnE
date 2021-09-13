c7ad125d084d81a93a7ee1ceb5a1ba1c6874a977
mqu <meitongdepaul@gmail.com>
2021-09-13 07:54:00 -0700

    working on viewing listings

--- frontend/src/store/listings.js
+++ frontend/src/store/listings.js
@@ -1,6 +1,8 @@
+import { bindActionCreators } from 'redux';
 import { csrfFetch } from './csrf';
 
 const CREATE_LISTING = 'listing/createListing';
+const GET_LISTING = 'listing/getListing';
 
 // create new listing/spot
 const createListingAction = (listing) => {
@@ -10,7 +12,13 @@ const createListingAction = (listing) => {
     };
 };
 
-
+// view listing/spot that current loggin user posted
+const getListingAction = (listings) => {
+    return {
+        type: GET_LISTING,
+        payload: listings,
+    };
+};
 
 // create new listing/spot
 export const createListingThunk = (listing) => async (dispatch) => {
@@ -24,8 +32,22 @@ export const createListingThunk = (listing) => async (dispatch) => {
     return response;
 };
 
+// view listing/spot that current loggin user posted
+export const getListingThunk = () => async (dispatch) => {
+
+    const response = await csrfFetch("/api/listings");
+    const data = await response.json();
+    dispatch(getListingAction(data.listings));
+    return response;
+};
 
 const initialState = {};
+
+const sortList = (list) => {
+    return list.sort((listingA, listingB) => {
+        return listingA.createdAt - listingB.createdAt;
+    }).map((listing) => listing.id);
+};
 
 const listingReducer = (state = initialState, action) => {
     let newState;
@@ -37,6 +59,19 @@ const listingReducer = (state = initialState, action) => {
                 [action.payload.id]: action.payload
             }
             return newState;
+
+        case GET_LISTING:
+            const allListings = {};
+            action.payload.forEach(listing=>{
+                allListings[listing.id]=listing;
+            })
+            newState = {
+                ...state,
+                ...allListings, 
+                listingsIds: sortList(action.payload),
+            }
+            return newState;
+
         default:
             return state;
     }
