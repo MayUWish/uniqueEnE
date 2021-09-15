910203e787755025c8946d932a2e223b548baed8
mqu <meitongdepaul@gmail.com>
2021-09-15 10:38:36 -0700

    finish create booking API route and validation

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -22,16 +22,91 @@ const validateBooking = [
     handleValidationErrors,
 ];
 
-// create bookings
+// create bookings, require user to log-in;
 router.post(
     '/',
     validateBooking,
     requireAuth,
-    asyncHandler(async (req, res) => {
-        const booking = await Booking.create({ ...req.body });
-        return res.json({
-            booking,
-        });
+    asyncHandler(async (req, res,next) => {
+        let { startDate, endDate, userId, listingId, numGuests } = req.body;
+        // eliminate hour minute second ms.
+        startDate.setHours(0, 0, 0, 0);
+        endDate.setHours(0, 0, 0, 0);
+        
+        const user = await User.findByPk(userId);
+        const listing = await Listing.findByPk(listingId);
+
+        // to check if booking date has conflict with existing bookings
+        const existingBookings = await Booking.findAll({where:{listingId}});
+        let isDateConflicted = existingBookings.some(booking => (booking.startDate.setHours(0, 0, 0, 0) <= startDate && booking.endDate.setHours(0, 0, 0, 0) >= startDate) || (booking.startDate.setHours(0, 0, 0, 0) <= endDate && booking.endDate.setHours(0, 0, 0, 0) >= endDate))
+
+        //array of array [startDate,endDate]
+        const existingBookingDates = existingBookings.map(booking=>[booking.startDate,booking.endDate]);
+        
+        if (+req.user.id !== +userId){
+            //logged-in userId is different from booking userId, which means book for others
+            const err = Error('Bad request.');
+            err.errors = [`You cannot book for other users.`];
+            err.status = 400;
+            err.title = 'Bad request.';
+            next(err);
+
+        }
+        else if(!listing){
+            // no such listing
+            const err = Error('Bad request.');
+            err.errors = [`The listing does not exist.`];
+            err.status = 400;
+            err.title = 'Bad request.';
+            next(err);
+
+        }
+        else if (!user ) {
+            // no such user
+            const err = Error('Bad request.');
+            err.errors = [`The user/account does not exist.`];
+            err.status = 400;
+            err.title = 'Bad request.';
+            next(err);
+
+        }
+        else if(+user.id===+listing.userId){
+            // userId cannot be host's userId
+            const err = Error('Bad request.');
+            err.errors = [`Please do not book your own listing.`];
+            err.status = 400;
+            err.title = 'Bad request.';
+            next(err);
+        } else if (isDateConflicted){
+            // to check if booking date has conflict with existing bookings
+            const message = existingBookingDates.map(date => `${date[0].toLocaleDateString()} - ${date[1].toLocaleDateString()}`)
+            const err = Error('Bad request.');
+            err.errors = [`Conflicts with the following date range of existing bookings.`,...message];
+            err.status = 400;
+            err.title = 'Bad request.';
+            next(err);
+
+        } else if (endDate <= startDate){
+            // check endDate more than startDate
+
+            const err = Error('Bad request.');
+            err.errors = [`End Date must be more than start date.`];
+            err.status = 400;
+            err.title = 'Bad request.';
+            next(err);
+
+        }
+
+        else{
+
+            const booking = await Booking.create({ startDate, endDate, userId, listingId, numGuests });
+            return res.json({
+                booking,
+            });
+
+        }
+
+        
     }),
 );
 
