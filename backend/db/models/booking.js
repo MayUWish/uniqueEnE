9e4b488335a7ecc148bff0e2380d69dbb6b9dc23
mqu <meitongdepaul@gmail.com>
2021-09-16 11:09:12 -0700

    fix bug for eager loading booking> Listing> image

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -1,6 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Booking = sequelize.define('Booking', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     listingId: DataTypes.INTEGER,
     userId: DataTypes.INTEGER,
     numGuests: DataTypes.INTEGER,
