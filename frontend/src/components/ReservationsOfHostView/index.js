2de1a86bb226b8c94d38a0f1595acceb4f3c7e84
mqu <meitongdepaul@gmail.com>
2021-09-17 12:48:29 -0700

    fix console warning about unique key

--- frontend/src/components/ReservationsOfHostView/index.js
+++ frontend/src/components/ReservationsOfHostView/index.js
@@ -13,16 +13,17 @@ const Reservations = ({ listings }) => {
         
         <div  style={{ width: '50%',  marginLeft: '1%', display:'flex',flexDirection:'column' }}>
             {listings?.map(listing => (
-                listing.Bookings.length>0 &&<div style={{border:'1px solid #d3d3d3', margin:'2%', borderRadius:'10px'}}>
+                listing.Bookings.length>0 &&<div key ={listing.id} style={{border:'1px solid #d3d3d3', 
+                margin:'2%', borderRadius:'10px'}}>
                     <h3>{listing.title}</h3>
                     <h4 style={{marginLeft:'5%'}}>{listing.address},{listing.city}</h4>
                     <h4 style={{ marginLeft: '5%' }}>Reservations:</h4>
                     {listing.Bookings.map(booking=>(
-                        <>  
+                        <div key={booking.id}>
                             <p style={{ marginLeft: '8%' }}>{`> by`} {booking.User.username}</p>
                             <p style={{ marginLeft: '10%' }}>from {booking.startDate.slice(0, 10)} to {booking.endDate.slice(0, 10)}</p>
                             
-                        </>
+                        </div>
                     ))}
                 </div>
                 ))}
