b4b9398bbc9eb26822abb40a2cff220f8d757abe
mqu <meitongdepaul@gmail.com>
2021-11-17 10:44:48 -0800

    display only conficted dates for booking

--- frontend/src/components/Bookings/PastBooking.js
+++ frontend/src/components/Bookings/PastBooking.js
@@ -13,7 +13,7 @@ const Booking = ({ booking}) => {
             <div className='intro'>
                 <h3>{booking.Listing?.title}</h3>
                 <p>{booking.Listing?.address}, {booking.Listing?.city}</p>
-                <p>{Number(booking.Listing?.twinBedNum) + Number(booking.Listing?.queenBedNum) + Number(booking.Listing?.kingBedNum) + Number(booking.Listing?.sofaBedNum)} beds , {booking.Listing?.bathroomNum} baths</p>
+                <p>{Number(booking.Listing?.twinBedNum) + Number(booking.Listing?.queenBedNum) + Number(booking.Listing?.kingBedNum) + Number(booking.Listing?.sofaBedNum)} beds, {booking.Listing?.bathroomNum} baths</p>
 
                 <h4>{booking.numGuests} guests' reservation:  </h4>
                 <h4 >from {booking.startDate.slice(0, 10)} to {booking.endDate.slice(0, 10)}</h4>
